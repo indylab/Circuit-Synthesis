436ec4fa70969cf455ab0e091c91185e3c31d281
Thomas Reeves <tjtommy5000@gmail.com>
2022-07-20 18:34:24 -0700

    Added TwoStageAmp circuit

--- NgSpicePipeline/src/NgSpiceTraining.py
+++ NgSpicePipeline/src/NgSpiceTraining.py
@@ -10,11 +10,12 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from Simulator import Simulator
 
+
 def check_acc(y_hat, y, margins=None):
     if margins is None:
-        margins = [0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1]
+        margins = [0.01, 0.05, 0.1]
     a_err = (np.abs(y_hat - y))  # get normalized error
-    err = np.divide(a_err, y, out=a_err, where=y != 0)
+    err = np.divide(a_err, y, where=y != 0)
     assert (err.shape == y.shape)
 
     accs = []
@@ -28,10 +29,6 @@ def check_acc(y_hat, y, margins=None):
         print(f"{m}% num correct = {num_correct} / {num_samples}")
         accs.append(num_correct / num_samples)
 
-    # correct_idx = np.where(err < margin)
-    # num_part_correct = len(correct_idx[0])
-    # num_part_samples = y.shape[0] * y.shape[1]
-
     return accs
 
 
@@ -39,14 +36,14 @@ def simulate_points(paramater_preds, norm_perform, scaler, simulator):
     data = np.hstack((paramater_preds, norm_perform))
     MAX_LENGTH = 750
     if data.shape[0] > MAX_LENGTH:
-        n = np.random.randint(0,paramater_preds.shape[0],MAX_LENGTH)
-        data = data[n,:]
-    unnorm_param_preds, unnorm_true_perform = scaler.inverse_transform(data)[:, :2], scaler.inverse_transform(
-        data)[:, 2:]
-    param1, param2 = unnorm_param_preds[:, 0], unnorm_param_preds[:, 1]
+        n = np.random.randint(0, paramater_preds.shape[0], MAX_LENGTH)
+        data = data[n, :]
+    unnorm_param_preds, unnorm_true_perform = scaler.inverse_transform(data)[:, :3], scaler.inverse_transform(
+        data)[:, 3:]
 
     _, y_sim = simulator.runSimulation(unnorm_param_preds)
-    assert y_sim.shape == norm_perform.shape or y_sim.shape[0] == MAX_LENGTH, f"simulation failed, {y_sim.shape} != {norm_perform.shape}"
+    assert y_sim.shape == norm_perform.shape or y_sim.shape[
+        0] == MAX_LENGTH, f"simulation failed, {y_sim.shape} != {norm_perform.shape}"
     accs = check_acc(y_sim, unnorm_true_perform)
     return accs
 
@@ -56,10 +53,12 @@ def train(model, train_data, val_data, optimizer, loss_fn, scaler, simulator, nu
     train_accs = []
     val_accs = []
     losses = []
-    train_acc = True
+    val_losses = []
+    train_acc = False
     for epoch in range(num_epochs):
         model.train()
         avg_loss = 0
+        val_avg_loss = 0
         for t, (x, y) in enumerate(train_data):
             # Zero your gradient
             optimizer.zero_grad()
@@ -76,8 +75,24 @@ def train(model, train_data, val_data, optimizer, loss_fn, scaler, simulator, nu
             optimizer.step()
 
         losses.append(avg_loss)
+        for t, (x, y) in enumerate(val_data):
+            # Zero your gradient
+
+            x_var = torch.autograd.Variable(x.type(torch.FloatTensor)).to(device)
+            y_var = torch.autograd.Variable(y.type(torch.FloatTensor).float()).to(device)
+            model.eval()
+            scores = model(x_var)
+
+            loss = loss_fn(scores.float(), y_var.float())
+
+            loss = torch.clamp(loss, max=500000, min=-500000)
+            val_avg_loss += (loss.item() - val_avg_loss) / (t + 1)
+
+        losses.append(avg_loss)
+        val_losses.append(val_avg_loss)
         if (epoch + 1) % print_every == 0:
             print('t = %d, loss = %.4f' % (epoch + 1, avg_loss))
+            print('t = %d, val loss = %.4f' % (epoch + 1, val_avg_loss))
 
         if epoch % 50 == 0:
             norm_perform, _ = val_data.dataset.getAll()
@@ -94,7 +109,7 @@ def train(model, train_data, val_data, optimizer, loss_fn, scaler, simulator, nu
                 train_accs.append(acc_list)
                 print(f"Training_Accuracy at Epoch {epoch} = {train_accs[-1][0]}")
 
-    return losses, train_accs,val_accs
+    return losses, val_losses, train_accs, val_accs
 
 
 if __name__ == '__main__':
@@ -104,16 +119,14 @@ if __name__ == '__main__':
 
     device = 'cuda:0' if cuda.is_available() else 'cpu'
     print(device)
-    model = models.Model50GELU(3, 2).to(device)
-    rerun_training = True
 
     ngspice_exec = "ngspice/Spice64/bin/ngspice.exe"
-    train_netlist = "NgSpicePipeline/assets/nmos-training.sp"
-    test_netlist = "NgSpicePipeline/assets/nmos-testing-pro.sp"
-    param_list = ["r", "w"]
-    perform_list = ["bw", "pw", "a0"]
+    train_netlist_nmos = "NgSpicePipeline/assets/nmos-training.sp"
+    test_netlist_nmos = "NgSpicePipeline/assets/nmos-testing-pro.sp"
+    param_list_nmos = ["r", "w"]
+    perform_list_nmos = ["bw", "pw", "a0"]
 
-    arguments = {
+    arguments_nmos = {
         "model_path": "NgSpicePipeline/assets/45nm_CS.pm",
         "w_start": 620,
         "w_stop": 1450,
@@ -123,25 +136,73 @@ if __name__ == '__main__':
         "r_change": "0.3750u",
         "out": "NgSpicePipeline/out/"
     }
-    simulator = Simulator(ngspice_exec, train_netlist, test_netlist, param_list, perform_list, arguments)
+    simulator_nmos = Simulator(ngspice_exec, train_netlist_nmos, test_netlist_nmos, param_list_nmos, perform_list_nmos,
+                               arguments_nmos)
 
+    train_netlist_cascade = "NgSpicePipeline/assets/nmos-training-cascode.sp"
+    test_netlist_cascade = "NgSpicePipeline/assets/nmos-testing-cascode.sp"
+    param_list_cascade = ["r", "w0", "w1"]
+    perform_list_cascade = ["bw", "pw", "a0"]
 
+    arguments_cascade = {
+        "model_path": "NgSpicePipeline/assets/45nm_CS.pm",
+        "w0_start": 620,
+        "w0_stop": 1450,
+        "w0_change": 50,
+        "w1_start": 620,
+        "w1_stop": 1450,
+        "w1_change": 50,
+        "r_start": "2.88u",
+        "r_stop": "6.63u",
+        "r_change": "0.7500u",
+        "out": "NgSpicePipeline/out/"
+    }
+    simulator_cascade = Simulator(ngspice_exec, train_netlist_cascade, test_netlist_cascade, param_list_cascade,
+                                  perform_list_cascade,
+                                  arguments_cascade)
+
+    train_netlist_two_stage = "NgSpicePipeline/assets/TwoStageAmplifier.sp"
+    test_netlist_two_stage = "NgSpicePipeline/assets/TwoStageAmplifier-Test.sp"
+    param_list_two_stage = ["w0", "w1", "w2"]
+    perform_list_two_stage = ["bw", "pw", "a0"]
+
+    arguments_two_stage = {
+        "model_path": "NgSpicePipeline/assets/45nm_CS.pm",
+        "w0_start": "25u",
+        "w0_stop": "30u",
+        "w0_change": "0.5u",
+        "w2_start": "52u",
+        "w2_stop": "55.5u",
+        "w2_change": "0.5u",
+        "w1_start": "6u",
+        "w1_stop": "9u",
+        "w1_change": "0.5u",
+        "out": "NgSpicePipeline/out/"
+    }
+    simulator_two_stage = Simulator(ngspice_exec, train_netlist_two_stage, test_netlist_two_stage, param_list_two_stage,
+                                    perform_list_two_stage,
+                                    arguments_two_stage)
+    simulator_two_stage.delete_existing_data = True
+
+    simulator = simulator_two_stage
+    rerun_training = True
     if rerun_training:
         x, y = simulator.run_training()
     else:
-        param_outfile_names = ["r.csv", "w.csv"]  # must be in order
+        param_outfile_names = ["w0.csv", "w1.csv", "w2.csv"]  # must be in order
         perform_outfile_names = ["bw.csv", "pw.csv", "a0.csv"]  # must be in order
         out = r"NgSpicePipeline/out/"
         x, y = simulator.getData(param_outfile_names, perform_outfile_names, out)
 
+    model = models.Model50GELU(3, 3).to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     loss_fn = torch.nn.L1Loss()
-    print(x.shape,y.shape)
+    print(x.shape, y.shape)
     data = np.hstack((x, y))
     scaler_arg = MinMaxScaler()
     scaler_arg.fit(data)
     data = scaler_arg.transform(data)
-    param, perform = data[:, :2], data[:, 2:]
+    param, perform = data[:, :3], data[:, 3:]
     print(param.shape, perform.shape)
     X_train, X_test, y_train, y_test = train_test_split(perform, param, test_size=0.1)
     print(X_train.shape, y_train.shape)
@@ -152,11 +213,20 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(train_data, batch_size=100)
     val_dataloader = DataLoader(val_data, batch_size=100)
 
-    losses, val_accs, train_accs = train(model, train_dataloader, val_dataloader, optimizer, loss_fn, scaler_arg,simulator, num_epochs=1000)
+    losses, val_losses, train_accs, val_accs = train(model, train_dataloader, val_dataloader, optimizer, loss_fn,
+                                                     scaler_arg,
+                                                     simulator, num_epochs=1000)
 
     plt.plot(range(len(losses)), losses)
+    plt.xlabel("epochs")
+    plt.ylabel("train loss")
+    plt.show()
+    plt.plot(range(len(val_losses)), val_losses)
+    plt.xlabel("epochs")
+    plt.ylabel("validation loss")
     plt.show()
     plt.plot(range(len(val_accs)), val_accs)
-    plt.show()
-    plt.plot(range(len(train_accs)), train_accs)
+    plt.xlabel("epochs")
+    plt.ylabel("validation accuracy")
+    plt.legend(['1%', '5%', '10%'])
     plt.show()
--- NgSpicePipeline/src/Simulator.py
+++ NgSpicePipeline/src/Simulator.py
@@ -21,11 +21,15 @@ class Simulator:
         self.test_param_filenames = [str(x) + "-test.csv" for x in parameter_list]
         self.test_perform_filenames = [str(x) + "-test.csv" for x in performance_list]
 
+        self.delete_existing_data = False
         # validate arguments
         for p in parameter_list:
             assert (str(p) + "_start" in arguments.keys()), ("Each paramater must have a start index", arguments.keys())
             assert (str(p) + "_stop" in arguments.keys()), ("Each paramater must have a start index", arguments.keys())
-            assert (str(p) + "_change" in arguments.keys()), ("Each paramater must have a start index", arguments.keys())
+            assert (str(p) + "_change" in arguments.keys()), (
+            "Each paramater must have a start index", arguments.keys())
+
+        self.save_error_log = False
 
     def _updateFile(self, trainingFilePath, outputFilePath, argumentMap):
         with open(trainingFilePath, 'r') as read_file:
@@ -42,14 +46,13 @@ class Simulator:
         files = []
         for file in filenames:
             file_data = pd.read_csv(file, header=None)
-            files.append(file_data.apply(lambda x: re.split(r"\s+", str(x))[2], axis=1))
-
+            file_data = file_data.apply(lambda x: re.split(r"\s+", str(x).replace("=",""))[2], axis=1)
+            files.append(file_data)
         combine = pd.concat(files, axis=1)
         return np.array(combine, dtype=float)
 
     def getData(self, param_outfile_names, perform_outfile_names, out):
         param_fullname = [os.path.join(out, file) for file in param_outfile_names]
-        print(param_fullname)
         perform_fullname = [os.path.join(out, file) for file in perform_outfile_names]
         x = self._convert(param_fullname)
         y = self._convert(perform_fullname)
@@ -67,32 +70,71 @@ class Simulator:
         assert parameters.shape[1] == len(self.parameter_list), f"list of points to simulate should be same length " \
                                                                 f"as number of parameters {parameters.shape[1]} != " \
                                                                 f"{len(self.parameter_list)} "
-        print(self.parameter_list)
+
+        if self.delete_existing_data:
+            self._delete_training_files()
+            self._delete_testing_files()
+
         updated_netlist_filename = self.test_netlist + "-formatted"
         argumentMap = self.arguments
         argumentMap["num_samples"] = parameters.shape[0]
         for i, p in enumerate(self.parameter_list):
             argumentMap[f"{p}_array"] = " ".join(list(parameters[:, i].astype(str)))
-        print(argumentMap)
         self._updateFile(self.test_netlist, updated_netlist_filename, argumentMap)
-
-        subprocess.run([self.ngspice_exec, '-r', 'rawfile.raw', '-b', '-i', updated_netlist_filename])
-        print(self.test_param_filenames)
+        if self.save_error_log:
+            args = [self.ngspice_exec, '-r', 'rawfile.raw', '-b', "-o", os.path.join(self.arguments["out"]), '-i',
+                    updated_netlist_filename]
+        else:
+            args = [self.ngspice_exec, '-r', 'rawfile.raw', '-b', '-i', updated_netlist_filename]
+        subprocess.run(args)
         x, y = self.getData(self.test_param_filenames, self.test_perform_filenames, argumentMap["out"])
         return [x, y]
 
     def run_training(self):
+        if self.delete_existing_data:
+            self._delete_training_files()
+            self._delete_testing_files()
+
         formatted_netlist = self.train_netlist + "-formatted"
         self._updateFile(self.train_netlist, formatted_netlist, self.arguments)
-
-        subprocess.run([self.ngspice_exec, '-r', 'rawfile.raw', '-b', '-i', formatted_netlist])
-
+        if self.save_error_log:
+            args = [self.ngspice_exec, '-r', 'rawfile.raw', '-b', '-i', "-o",
+                    os.path.join(self.arguments["out"], "log.txt"), formatted_netlist]
+        else:
+            args = [self.ngspice_exec, '-r', 'rawfile.raw', '-b', '-i', formatted_netlist]
+        subprocess.run(args)
         x, y = self._getData(self.train_param_filenames, self.train_perform_filenames, self.arguments["out"])
 
         return x, y
 
+    def _delete_training_files(self):
+        out = self.arguments["out"]
+        param_fullname = [os.path.join(out, file) for file in self.train_param_filenames]
+        perform_fullname = [os.path.join(out, file) for file in self.train_perform_filenames]
 
-if __name__ == '__main__':  # TODO: remove print statements
+        for file in (param_fullname + perform_fullname):
+            try:
+                os.remove(file)
+            except FileNotFoundError:
+                continue
+
+    def _delete_testing_files(self):
+        out = self.arguments["out"]
+        param_fullname = [os.path.join(out, file) for file in self.test_param_filenames]
+        perform_fullname = [os.path.join(out, file) for file in self.test_perform_filenames]
+
+        for file in (param_fullname + perform_fullname):
+            try:
+                os.remove(file)
+            except FileNotFoundError:
+                continue
+
+
+if __name__ == '__main__':
+    # TODO: remove print statements
+    # TODO: find work around for too many arguments crashing simulator
+    # TODO: find way to include simulator console logging for easier debugging (try -o output filename)
+    # TODO: connect with training
     ngspice_exec = "ngspice/Spice64/bin/ngspice.exe"
     train_netlist = "NgSpicePipeline/assets/nmos-training-2.sp"
     test_netlist = "NgSpicePipeline/assets/nmos-testing-pro.sp"
@@ -109,10 +151,9 @@ if __name__ == '__main__':  # TODO: remove print statements
         "change2": 5.5,
         "out": "NgSpicePipeline/out/"
     }
-    #TODO: find work around for too many arguments crashing simulator
-    #TODO: find way to include simulator console logging for easier debugging (try -o output filename)
-    #TODO: connect with training
-    simulator = Simulator(ngspice_exec, train_netlist, test_netlist, param_list, perform_list, arguments)
+
+    simulator_nmos = Simulator(ngspice_exec, train_netlist, test_netlist, param_list, perform_list, arguments)
+    simulator = simulator_nmos
     rerun_training = True
     if rerun_training:
         x, y = simulator.run_training()
